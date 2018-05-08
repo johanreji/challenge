5575b1e030dc030b2cb279845323f7e0ad013298
johanreji <johanpenatu@gmail.com>
2018-05-08 11:37:42 +0530

    init

---
+++ Frontend-Summer-2018-master/demo.js
@@ -0,0 +1,8 @@
+//Insert code below
+
+checkname(){
+	let fname=document.getElementsByTagName('input')[0];
+	let lname=document.getElementsByTagName('input')[1];
+	if(fname.match(0/1/2/3/4/5/6/7/8/9) || fname.match(0/1/2/3/4/5/6/7/8/9)	)
+		alert("error in names");
+}
